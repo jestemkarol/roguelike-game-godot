e7f880880d46c5fa8da27fbf3776b6af9e3eeeec
Karol Wyliziński <kwylizinski@gmail.com>
2024-08-05 20:54:05 +0200

    Basic atatck / health actions for player/ enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,23 +1,43 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var just_hit_timer: Timer = $just_hit_timer
+@onready var animation_player: AnimatedSprite2D = $AnimatedSprite2D
+@onready var attack_cooldown_timer: Timer = $attack_cooldown_timer
+
 @export var speed = 50
+@export var health = 50
 var player_chase = false
 var player = null
 var direction = Vector2.ZERO
+var just_hit = false
+var attack_cooldown_on = false
+var is_attacking = false
+var dead = false
 
 func _ready() -> void:
-	$AnimatedSprite2D.play('idle_down')
+	animation_player.play('idle_down')
 
 func _physics_process(delta: float) -> void:
+	if dead:
+		return
+	if is_attacking:
+		attack_cooldown_on = true
+		attack_cooldown_timer.start()
+		play_animation('attack')
+		attack_charge()
+		player.hit(10, direction)
 	if player_chase:
-		direction = (player.get_global_position() - position).normalized()
-		velocity = direction * speed * delta
-		play_animation(direction)
+		if !is_attacking:
+			direction = (player.get_global_position() - position).normalized()
+			velocity = direction * speed * delta
+			play_animation('move')
 	else:
 		velocity = lerp(velocity, Vector2.ZERO, 0.07)
 	move_and_collide(velocity)
+
 func _on_detection_area_body_entered(body: Node2D) -> void:
+	if dead:
+		return
 	if body.is_in_group('player'):
 		player = body
 		player_chase = true
@@ -27,14 +47,52 @@ func _on_detection_area_body_exited(body: Node2D) -> void:
 		player = null
 		player_chase = false
 		
-func play_animation(direction: Vector2) -> void:
-	$AnimatedSprite2D.flip_h = false
+func play_animation(type: String) -> void:
+	animation_player.flip_h = false
 	if direction.y > direction.x && direction.y > -direction.x:
-		$AnimatedSprite2D.play('move_down')
+		animation_player.play('%s_down' % type)
 	elif direction.y > direction.x && direction.y < -direction.x:
-		$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play('move_side')
+		animation_player.flip_h = true
+		animation_player.play('%s_side' % type)
 	elif direction.y < direction.x && direction.y < -direction.x:
-		$AnimatedSprite2D.play('move_up')
+		animation_player.play('%s_up' % type)
 	elif direction.y < direction.x && direction.y > -direction.x:
-		$AnimatedSprite2D.play('move_side')
+		animation_player.play('%s_side' % type)
+
+func hit(damage: int):
+	if !just_hit:
+		just_hit = true
+		just_hit_timer.start()
+		health -= damage
+		print("Enemy health: %s" % health)
+		if health < 0:
+			death()
+		knockback()
+
+func knockback():
+	var tween = create_tween()
+	tween.tween_property(self, 'global_position', global_position - direction * 25, 0.2)
+
+func attack_charge():
+	var tween = create_tween()
+	tween.tween_property(self, 'global_position', global_position + direction * 12, 0.2)
+
+func death():
+	dead = true
+	animation_player.play('death')
+	await get_tree().create_timer(3).timeout
+	queue_free()
+
+func _on_just_hit_timer_timeout() -> void:
+	just_hit = false
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group('player'):
+		is_attacking = true
+		player = body
+
+func _on_attack_cooldown_timer_timeout() -> void:
+	attack_cooldown_on = false
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	is_attacking = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,67 +1,110 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimatedSprite2D = $AnimatedSprite2D
+@onready var just_hit_timer: Timer = $just_hit_timer
+@onready var deal_attack_timer: Timer = $deal_attack_timer
+@onready var player_hitbox: Area2D = $player_hitbox
+
 @export var speed = 100
+@export var health = 100
 var current_dir = 'down'
+var enemy_in_attack_range = false
+var just_hit = false
+var is_attacking = false
+var dead = false
+var enemy = null
+var knockback_direction = Vector2.ZERO
+var knocked_back = false
 
 func _ready() -> void:
-	$AnimatedSprite2D.play('idle_down')
+	animation_player.play('idle_down')
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
 	
 func player_movement(delta: float) -> void:
+	if Input.is_action_pressed('attack'):
+		attack()
 	if Input.is_action_pressed('move_right'):
-		play_animation(1)
-		current_dir = 'right'
-		velocity.x = speed
-		velocity.y = 0
+		velocity = movement('right', 'move', speed, 0)
 	elif Input.is_action_pressed('move_left'):
-		play_animation(1)
-		current_dir = 'left'
-		velocity.x = -speed
-		velocity.y = 0
+		velocity = movement('left', 'move', -speed, 0)
 	elif Input.is_action_pressed('move_down'):
-		play_animation(1)
-		current_dir = 'down'
-		velocity.x = 0
-		velocity.y = speed
+		velocity = movement('down', 'move', 0, speed)
 	elif Input.is_action_pressed('move_up'):
-		play_animation(1)
-		current_dir = 'up'
-		velocity.x = 0
-		velocity.y = -speed
+		velocity = movement('up', 'move', 0, -speed)
 	else:
-		play_animation(0)
-		velocity.x = 0
-		velocity.y = 0
-		
+		velocity = idle()
 	move_and_slide()
 
-func play_animation(movement):
+func attack() -> void:
+	is_attacking = true
+	deal_attack_timer.start()
+	play_animation('attack')
+	if enemy:
+		enemy.hit(20)
+
+func movement(direction: String, animation_type: String, x: int, y: int) -> Vector2:
+	if is_attacking:
+		return Vector2.ZERO
+	switch_hitbox_shape(direction)
+	current_dir = direction
+	play_animation(animation_type)
+	return Vector2(x, y)
+
+func switch_hitbox_shape(direction):
+	for shape in player_hitbox.get_children():
+		shape.disabled = true
+	player_hitbox.get_node(direction).disabled = false
+
+func idle() -> Vector2:
+	if is_attacking:
+		return Vector2.ZERO
+	play_animation('idle')
+	return Vector2.ZERO
+
+func play_animation(type):
 	var dir = current_dir
-	
-	if dir == 'right':
-		$AnimatedSprite2D.flip_h = false
-		if movement == 1:
-			$AnimatedSprite2D.play('move_side')
-		elif movement == 0:
-			$AnimatedSprite2D.play('idle_side')
-	if dir == 'left':
-		$AnimatedSprite2D.flip_h = true
-		if movement == 1:
-			$AnimatedSprite2D.play('move_side')
-		elif movement == 0:
-			$AnimatedSprite2D.play('idle_side')
-	if dir == 'down':
-		$AnimatedSprite2D.flip_h = false
-		if movement == 1:
-			$AnimatedSprite2D.play('move_down')
-		elif movement == 0:
-			$AnimatedSprite2D.play('idle_down')
-	if dir == 'up':
-		$AnimatedSprite2D.flip_h = false
-		if movement == 1:
-			$AnimatedSprite2D.play('move_up')
-		elif movement == 0:
-			$AnimatedSprite2D.play('idle_up')
+	if current_dir == 'left':
+		animation_player.flip_h = true
+		dir = 'side'
+	elif current_dir == 'right':
+		animation_player.flip_h = false
+		dir = 'side'
+	animation_player.play("%s_%s" % [type, dir])
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group('enemy'):
+		enemy = body
+
+func hit(damage: int, hit_direction: Vector2):
+	if !just_hit:
+		just_hit = true
+		just_hit_timer.start()
+		health -= damage
+		knockback_direction = hit_direction
+		print("Player health: %s" % health)
+		if health < 0:
+			death()
+
+func knockback():
+	if knocked_back:
+		return
+	knocked_back = true
+	var tween = create_tween()
+	tween.tween_property(self, 'global_position', global_position + knockback_direction * 15, 0.2)
+	await get_tree().create_timer(0.2).timeout
+	knocked_back = false
+
+func death():
+	print('bye')
+
+func _on_just_hit_timer_timeout() -> void:
+	just_hit = false
+
+func _on_deal_attack_timer_timeout() -> void:
+	is_attacking = false
+
+func _on_player_knockback_hitbox_body_entered(body: Node2D) -> void:
+	if body.is_in_group('enemy') && just_hit:
+		knockback()
